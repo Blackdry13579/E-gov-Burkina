f1248d7d89733f037d338d8a383072f1267800f8
Landry ZONGO <zongolandry793@gmail.com>
2026-03-25 17:51:25 +0000

    Chore: point mobile app to live Render backend

--- frontend-mobile/lib/core/constants/api_config.dart
+++ frontend-mobile/lib/core/constants/api_config.dart
@@ -1,10 +1,9 @@
 
 class ApiConfig {
-  // URL Ngrok pour test externe stable
-  static const String tunnelUrl = 'https://nonmicrobic-lorean-nonhabitually.ngrok-free.dev/api';
+  // URL de production sur Render
+  static const String tunnelUrl = 'https://e-gov-burkina-api.onrender.com/api';
 
   static String get baseUrl {
-    // Si on utilise Localtunnel, on renvoie l'URL du tunnel directement
     return tunnelUrl;
   }
 }
